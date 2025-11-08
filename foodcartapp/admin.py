d757c115451e932b17a22dbe982686c0adf9dcca
NadyaEvp <t_evp@mail.ru>
2025-11-08 20:39:49 +0300

    Добавляем фичу

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.shortcuts import reverse
+from django.http import HttpResponseRedirect
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Product
 from .models import ProductCategory
@@ -9,6 +11,8 @@ from .models import Restaurant
 from .models import RestaurantMenuItem
 from .models import Order
 from .models import OrderItem
+
+from star_burger.settings import ALLOWED_HOSTS
 
 
 class RestaurantMenuItemInline(admin.TabularInline):
@@ -95,7 +99,6 @@ class ProductAdmin(admin.ModelAdmin):
         return format_html('<img src="{url}" style="max-height: 200px;"/>', url=obj.image.url)
     get_image_preview.short_description = 'превью'
 
-
     def get_image_list_preview(self, obj):
         if not obj.image or not obj.id:
             return 'нет картинки'
@@ -128,3 +131,14 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [
         OrderItemInline
     ]
+
+    def response_change(self, request, obj):
+        next_url = request.GET.get('next')
+        if next_url and url_has_allowed_host_and_scheme(
+            url=next_url,
+            allowed_hosts={request.get_host()},
+            require_https=request.is_secure()
+        ):
+            return HttpResponseRedirect(next_url)
+
+        return super().response_change(request, obj)
